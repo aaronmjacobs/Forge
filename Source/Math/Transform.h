8e0136e3ea8c51e9e0cc9e4f4e1e72f0b1b5adf2
Aaron Jacobs <aaron@aaronmjacobs.com>
2021-01-18 23:58:22 -0800

    Add Transform utility functions

--- Source/Math/Transform.h
+++ Source/Math/Transform.h
@@ -42,9 +42,39 @@ struct Transform
       return orientation * vector;
    }
 
+   glm::vec3 getForwardVector() const
+   {
+      return rotateVector(MathUtils::kForwardVector);
+   }
+
+   glm::vec3 getRightVector() const
+   {
+      return rotateVector(MathUtils::kRightVector);
+   }
+
+   glm::vec3 getUpVector() const
+   {
+      return rotateVector(MathUtils::kUpVector);
+   }
+
    Transform inverse() const;
    Transform relativeTo(const Transform& other) const;
 
    void operator*=(const Transform& other);
    Transform operator*(const Transform& other) const;
+
+   void rotateBy(const glm::quat& rotation)
+   {
+      orientation = rotation * orientation;
+   }
+
+   void translateBy(const glm::vec3& translation)
+   {
+      position += translation;
+   }
+
+   void scaleBy(const glm::vec3& scaleValue)
+   {
+      scale *= scaleValue;
+   }
 };
