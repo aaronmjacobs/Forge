95c895de27bffba184edc80908addc5e1fbe892a
Aaron Jacobs <aaron@aaronmjacobs.com>
2022-05-25 21:51:29 -0700

    Remove old MoltenVK workaround

--- Source/Renderer/Passes/Depth/DepthPass.cpp
+++ Source/Renderer/Passes/Depth/DepthPass.cpp
@@ -67,13 +67,6 @@ void DepthPass::render(vk::CommandBuffer commandBuffer, const SceneRenderInfo& s
 
    if (isShadowPass)
    {
-#if defined(__APPLE__)
-      PipelineDescription<DepthPass> description;
-      description.cubemap = true;
-      // Workaround for bug in MoltenVK - need to set the pipeline before setting the depth bias
-      commandBuffer.bindPipeline(vk::PipelineBindPoint::eGraphics, getPipeline(description).getVkPipeline());
-#endif // defined(__APPLE__)
-
       const ViewInfo& viewInfo = sceneRenderInfo.view.getInfo();
       commandBuffer.setDepthBias(viewInfo.depthBiasConstantFactor, viewInfo.depthBiasClamp, viewInfo.depthBiasSlopeFactor);
    }
