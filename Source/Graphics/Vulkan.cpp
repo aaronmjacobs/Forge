f9816eda24584286e9225f39e218db9b7b3099ed
Aaron Jacobs <aaron@aaronmjacobs.com>
2023-10-25 17:30:29 -0700

    Ignore nullability warnings in vk_mem_alloc.h

--- Source/Graphics/Vulkan.cpp
+++ Source/Graphics/Vulkan.cpp
@@ -2,6 +2,15 @@
 #  define WIN32_LEAN_AND_MEAN
 #endif
 
+#if defined(__clang__)
+#  pragma clang diagnostic push
+#  pragma clang diagnostic ignored "-Wnullability-completeness" // Pointer is missing a nullability type specifier (_Nonnull, _Nullable, or _Null_unspecified)
+#endif // defined(__clang__)
+
 // Must include directly (can't include Vulkan.h, as it is used in the PCH)
 #define VMA_IMPLEMENTATION
 #include <vk_mem_alloc.h>
+
+#if defined(__clang__)
+#  pragma clang diagnostic pop
+#endif // defined(__clang__)
